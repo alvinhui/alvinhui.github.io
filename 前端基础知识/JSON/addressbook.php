e824ebbd6b87298fe3b307b1701840f2e1ec2bed
Alvin Xu <alvinxwt@gmail.com>
2013-08-10 18:31:21 +0800

    Json

---
+++ 前端基础知识/JSON/addressbook.php
@@ -0,0 +1,16 @@
+<?php
+    header("Content-Type: text/plain");
+?>
+[
+    {
+        "name": "Nicholas C. Zakas",
+        "email": "nicholas@some-domain-name.com"
+    },
+    {
+        "name": "Jim Smith",
+        "email": "jimsmith@some-domain-name.com"
+    },    {
+        "name": "Michael Jones",
+        "email": "mj@some-domain-name.com"
+    }
+]
---
+++ 前端基础知识/timeout.php
@@ -0,0 +1,4 @@
+<?php
+    header("Content-Type: text/plain");   
+    sleep(2000);
+?>
